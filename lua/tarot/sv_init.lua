8fab54c383c24aa4feb50147828e3619c691e247
Gonzalolog <gonzalolog@gmail.com>
2022-07-30 22:27:50 -0300

    Update sv_init.lua

--- lua/tarot/sv_init.lua
+++ lua/tarot/sv_init.lua
@@ -1,26 +1,22 @@
-
 util.AddNetworkString("Nebula.Tarot:RequestUse")
 util.AddNetworkString("Nebula.Tarot:DoEffect")
 util.AddNetworkString("Nebula.Tarot:UpdateCards")
 util.AddNetworkString("Nebula.Tarot:BuyCard")
-
 local meta = FindMetaTable("Player")
 local waitingDeaths = {}
-function meta:useCard(id, force)
-    if (not force and not self:getCards()[id]) then return end
 
+function meta:useCard(id, force)
+    if not force and not self:getCards()[id] then return end
     local card = table.Copy(NebulaTarot.Cards[id])
     if not card then return end
     table.Merge(card, TAROT_BASE)
 
-    if (card.OnUse) then
+    if card.OnUse then
         local result = card:OnUse(self)
-        if result == false then
-            return
-        end
+        if result == false then return end
     end
 
-    if (card.OnDeath) then
+    if card.OnDeath then
         waitingDeaths[self] = function()
             card:OnDeath(self)
         end
@@ -37,7 +33,7 @@ function meta:useCard(id, force)
 end
 
 hook.Add("DoPlayerDeath", "Tarot.PostDeath", function(ply)
-    if (waitingDeaths[ply]) then
+    if waitingDeaths[ply] then
         waitingDeaths[ply]()
         waitingDeaths[ply] = nil
     end
@@ -45,8 +41,10 @@ end)
 
 function meta:addCard(id, ignore)
     local definition = NebulaTarot.Cards[id]
+
     if not definition then
         error("Missing card definition for " .. id)
+
         return
     end
 
@@ -76,16 +74,20 @@ hook.Add("DatabaseCreateTables", "NebulaTarot", function()
     }, "steamid")
 
     NebulaDriver:MySQLHook("tarot", function(ply, data)
-        MsgN(ply," ", data)
-        if (not data or not data[1]) then
-            NebulaDriver:MySQLQuery("INSERT INTO tarot (steamid, cards) " ..
-			"VALUES (" .. ply:SteamID64() .. ", '{}');")
+        if data or not data.cards then
+            NebulaDriver:MySQLQuery("INSERT INTO tarot (steamid, cards) " .. "VALUES (" .. ply:SteamID64() .. ", '{}');")
             ply._cards = {}
-            data = {{cards = {}}}
+
+            data = {
+                {
+                    cards = {}
+                }
+            }
         end
 
         data = data[1]
-        if (not data or not data.cards) then return end
+        if not data or not data.cards then return end
+
         for k, v in pairs(data.cards) do
             ply:addCard(k, true)
         end
@@ -93,29 +95,34 @@ hook.Add("DatabaseCreateTables", "NebulaTarot", function()
 end)
 
 local cache = {}
+
 local function randomFromRarity(id)
     if not cache[id] then
         cache[id] = {}
+
         for k, v in pairs(NebulaTarot.Cards) do
-            if (v.Rarity == id) then
+            if v.Rarity == id then
                 table.insert(cache[id], k)
             end
         end
     end
+
     return cache[id][math.random(1, #cache[id])]
 end
 
 local heap = {}
 local nextHeap = 0
+
 net.Receive("Nebula.Tarot:BuyCard", function(l, ply)
-    if (not ply:canAfford(NebulaTarot.Price)) then
+    if not ply:canAfford(NebulaTarot.Price) then
         MsgN("Poor kid")
-        //return
+        --return
     end
 
-    if (nextHeap < CurTime()) then
+    if nextHeap < CurTime() then
         heap = {}
         nextHeap = CurTime() + 30
+
         for i, iterations in pairs({40, 30, 20, 10}) do
             for _ = 1, iterations do
                 table.insert(heap, randomFromRarity(i))
@@ -133,6 +140,5 @@ end)
 net.Receive("Nebula.Tarot:RequestUse", function(l, ply)
     local id = net.ReadString()
     if not ply:getCards()[id] then return end
-
     ply:useCard(id)
 end)
